299ee8b6903c22794fc5e05969f8d805766de19a
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2020-03-15 13:26:15 +0100

    asd

--- Assignment_4/Assignment_4/Assignment_4.py
+++ Assignment_4/Assignment_4/Assignment_4.py
@@ -1,17 +1,23 @@
-# TensorFLow and tf.keras
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+# TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow import keras.utils.to_categorical
-#from keras.utils import to_categorical
-
-print('tensorflow version: ', tf.VERSION)
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Helper libraries
 import numpy as np
-print ('numpy version: ', np.__version__)
-
 import matplotlib.pyplot as plt
+
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+print(tf.__version__)
+
+#batch_size = 128
+#epochs = 10
+IMG_HEIGHT = 28
+IMG_WIDTH = 28
 
 fashion_mnist = keras.datasets.fashion_mnist
 
@@ -24,134 +30,70 @@ print('train_labels shape: ', len(train_labels))
 print('train_images shape: ', test_images.shape)
 print('train_labels shape: ', len(test_labels))
 
-#plt.figure()
-#plt.imshow(train_images[0])
-#plt.colorbar()
-#plt.grid(False)
-#plt.show()
+train_images = train_images.reshape(60000, IMG_WIDTH, IMG_HEIGHT, 1)
+test_images = test_images.reshape(10000, IMG_WIDTH, IMG_HEIGHT, 1)
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        img = img.reshape(IMG_WIDTH,IMG_HEIGHT)
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
 
-#plt.figure(figsize=(10,10))
-#for i in range(25):
-#    plt.subplot(5,5,i+1)
-#    plt.xticks([])
-#    plt.yticks([])
-#    plt.grid(False)
-#    plt.imshow(train_images[i], cmap=plt.cm.binary)
-#    plt.xlabel(class_names[train_labels[i]])
-#plt.show()
+image_gen_train = ImageDataGenerator(rotation_range=45,
+                                     width_shift_range=.15,
+                                     height_shift_range=.15,
+                                     horizontal_flip=True,
+                                     zoom_range=0.5)
+validation_image_generator = keras.preprocessing.image.ImageDataGenerator() # Generator for our validation data
 
-train_images = train_images.reshape(6000,28,28,1)
-test_images = test_images.reshape(6000,28,28,1)
 
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+#image_gen_train.fit(train_images)                                   # adds generated images to the train_images 
+train_data_gen = image_gen_train.flow(train_images, train_labels)
+validation_data_gen = validation_image_generator.flow(test_images, test_labels)  
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-model.add(Conv2D(64, kernel_size=3, activation='relu'))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
 
-#model = keras.Sequential([
-   # keras.layers.Flatten(input_shape=(28, 28)),
-    #keras.layers.Dense(128, activation=tf.nn.relu),
-    #keras.layers.Dense(10, activation=tf.nn.softmax)
-    #])
-
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32, kernel_size=3, padding='same', activation='relu', input_shape=(IMG_WIDTH,IMG_HEIGHT,1)))
+model.add(keras.layers.MaxPooling2D((2, 2)))
+model.add(keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D((2, 2)))
+model.add(keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+model.summary()
+
+#history = model.fit(train_data_gen, steps_per_epoch = 60000 // batch_size, epochs = 10, validation_data=validation_data_gen, max_queue_size=50, workers=4, use_multiprocessing=False)
+
+history = model.fit(train_data_gen, epochs = 10, validation_data=validation_data_gen)
+history = model.fit(train_images, train_labels, validation_data=(test_images,test_labels), epochs=10)
+#history = model.fit(train_data_gen, steps_per_epoch = 60000 // batch_size, epochs = 10, validation_data=validation_data_gen)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('Test accuracy:', test_acc)
 
 predictions = model.predict(test_images)
 
-def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  
-  plt.imshow(img, cmap=plt.cm.binary)
-  
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-  
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
 
-def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array, true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-  
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+    
 
-  
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-
-# Plot the first X test images, their predicted label, and the true label
-# Color correct predictions in blue, incorrect predictions in red
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions[i], test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions[i], test_labels)
-plt.show()
-
-img = test_images[3]
-
-print(img.shape)
-
-
-# Add the image to a batch where it's the only member.
-img = (np.expand_dims(img,0))
-
-print(img.shape)
-
-predictions_single = model.predict(img)
-
-print(predictions_single)
-plot_value_array(3, predictions_single[0], test_labels)
-plt.xticks(range(10), class_names, rotation=45)
-plt.show()
-
-prediction_result = np.argmax(predictions_single[0])
-print(prediction_result)
 
